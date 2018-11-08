cf40e8d9e08032bef94305f16f7c5723f06f254b
eb247 <eb247@hotmail.com>
2018-11-08 16:43:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var container = [];
   for(var i = 0; i < musicians.length; i++){
-    container.push(musicians[i] + "plays" + instruments[i])
+    container.push(musicians[i] + " plays " + instruments[i])
   }
 
   return container
