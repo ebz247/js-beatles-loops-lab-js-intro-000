7e28959c018eeb8ec9fcd90151dad90c9c21afec
eb247 <eb247@hotmail.com>
2018-11-08 17:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ function iLoveTheBeatles(number){
   var foo = [];
   do {
     foo.push("I love the Beatles!")
-    number++;
-  } while(number < 15) {
     console.log("the difference is currently" + 15-number);
-  }
+    number++;
+  } while(number < 15)
   return foo;
 }
