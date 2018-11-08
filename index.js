48af540c4205abe4ee5ca9a77db7c298451726aa
eb247 <eb247@hotmail.com>
2018-11-08 16:39:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var container = [];
-  for(var i = 0; i <= musicians.length; i++){
+  for(var i = 0; i < musicians.length; i++){
 
   }
 
